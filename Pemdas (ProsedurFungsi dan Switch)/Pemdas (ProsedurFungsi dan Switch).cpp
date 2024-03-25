ab56175b895f9039fd94c7c5088914bc6999d7db
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2024-03-25 10:13:09 +0700

    add fungsi

--- Pemdas (ProsedurFungsi dan Switch)/Pemdas (ProsedurFungsi dan Switch).cpp
+++ Pemdas (ProsedurFungsi dan Switch)/Pemdas (ProsedurFungsi dan Switch).cpp
@@ -15,4 +15,22 @@ void input()
 
 }
 
+int penjumlahan(int bil_1, int bil_2) {
+	return bil_1 + bil_2;
 
+}
+
+int pengurangan(int bil_1, int bil_2) {
+	return bil_1 - bil_2;
+
+}
+
+int perkalian(int bil_1, int bil_2) {
+	return bil_1 * bil_2;
+
+}
+
+int pembagian(int bil_1, int bil_2) {
+	return bil_1 / bil_2;
+
+}
